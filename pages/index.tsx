d457019384b14ee4a3cd583f08deb9e01bb094a9
Sellab <102852787+BobThePogBuilder@users.noreply.github.com>
2024-03-10 15:21:40 -0700

    Added website logo index.tsx
    Added website logo among other QOL changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -184,6 +184,7 @@ export default function Home() {
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt=""
               /> */}
+              <span role="img" aria-label="Logo" className="font-bold text-2xl">🍵</span>
               <span className="font-bold text-2xl">🍵 Peppermint Labs</span>
             </a>
           </div>
